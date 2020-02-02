b048b77dbd8243e53bdcc8270d6826f277de9a61
Aida Abdikulova <abdikulova@gmail.com>
2020-02-02 23:47:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -51,8 +51,11 @@ def self.find_by_id(id)
   sql = <<-SQL
   SELECT * FROM dogs
   WHERE id = ?
+  LIMIT 1
   SQL
-  DB[:conn].execute(sql,id)
+  DB[:conn].execute(sql,id).map do |row|
+    self.new_from_db(row)
+  end.first
 end
 
 end
