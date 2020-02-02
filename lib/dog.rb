7d162943a4935f0ae8d693df48fe1dd6e702dd87
Aida Abdikulova <abdikulova@gmail.com>
2020-02-02 23:46:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -47,12 +47,12 @@ def self.new_from_db(row)
   new_dog
 end
 
-def self.find_by_id(value)
+def self.find_by_id(id)
   sql = <<-SQL
   SELECT * FROM dogs
   WHERE id = ?
   SQL
-  DB[:conn].execute(sql,value).flatten
+  DB[:conn].execute(sql,id)
 end
 
 end
